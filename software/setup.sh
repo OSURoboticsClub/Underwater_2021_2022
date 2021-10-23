af3fa0336a30180600019013fcf3d6ebd849a0b2
jwCheng28 <chengjw68@gmail.com>
2021-10-23 12:54:51 -0700

    Test version of setup script

---
+++ software/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+catkin_ws="$HOME/catkin_ws"
+catkin_src="$catkin_ws/src"
+
+dev_path="$HOME/Dev/Underwater_2021_2022"
+dev_ros_pkgs="$dev_path/software/ros_pkgs"
+
+symlinked_folders=$(find $catkin_src -maxdepth 1 -type l)
+if [ -n $symlinked_folders ]; then
+    echo "Removing existing symlinks"
+    rm $symlinked_folders
+fi
+
+# Make the new symbolic link connections
+echo "Making new symlinks."
+ros_pkgs=$(find $dev_ros_pkgs/* -maxdepth 0 -type d)
+for folder in $ros_pkgs; do
+    ln -s "$dev_ros_pkgs/$folder" "$catkin_src/."
+    echo "Adding symlink for $folder."
+done
