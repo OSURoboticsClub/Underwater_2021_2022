50c2b991bb6996bc4daf3b4c11663c24088548a7
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-04-03 10:45:33 -0700

    Topic transport (#2)
    Add basic topic transporting to ROV and ground station setups.

--- software/setup.sh
+++ software/setup.sh
@@ -7,6 +7,8 @@ dev_ros_pkgs="$dev_path/software/ros_pkgs"
 
 symlinked_folders=$(find $catkin_src -maxdepth 1 -type l)
 if [ -n $symlinked_folders ]; then
+    echo "Nothing to remove"
+else 
     echo "Removing existing symlinks"
     rm $symlinked_folders
 fi
@@ -15,6 +17,13 @@ fi
 echo "Making new symlinks."
 ros_pkgs=$(find $dev_ros_pkgs/* -maxdepth 0 -type d)
 for folder in $ros_pkgs; do
-    ln -s "$dev_ros_pkgs/$folder" "$catkin_src/."
+    ln -s "${folder}" "$catkin_src/"
     echo "Adding symlink for $folder."
 done
+
+cd "$catkin_ws"
+catkin_make
+
+source devel/setup.bash
+
+exit 0
