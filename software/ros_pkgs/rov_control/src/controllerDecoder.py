a3046faf935af3bde1e8b88b49b9bceedec3ccf6
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-11-13 16:36:57 -0800

    Update some control code. Hard to tell what to improve without the physical robot atm

--- software/ros_pkgs/rov_control/src/controllerDecoder.py
+++ software/ros_pkgs/rov_control/src/controllerDecoder.py
@@ -4,6 +4,7 @@ from smbus2 import SMBus, i2c_msg
 import rospy
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
+from math import sqrt, cos, sin
 
 #To-do: figure out if would improve performance to limit import to just datatype
 import numpy as np
@@ -14,16 +15,16 @@ msg = gamepad()
 
 motorControllAddr = 0x50
 
-def callback(data -> gamepad):
+def callback(data):
     #TO-DO: normalize
     DPadVert = data.Axis[0]
     RJoyHorz = data.Axis[1]
     RJoyVert = data.Axis[2]
-    RBumper  = data.Axis[3]
-    LJoyHorz = data.Axis[4]
-    LJoyVert = data.Axis[5]
+    RBumper  = (self.axis_states.values()[3] + 1.0) / 2.0
+    LJoyHorz = data.Axis[5]
+    LJoyVert = data.Axis[4]
     DPadHorz = data.Axis[6]
-    LBumper  = data.Axis[7]
+    LBumper  = (self.axis_states.values()[7] + 1.0) / 2.0
 
     A = data.Buttons[0]
     B = data.Buttons[1]
@@ -49,32 +50,61 @@ def callback(data -> gamepad):
     """
     motors = [0, 0, 0, 0, 0, 0]
     #left joystick X is roll
-    if(float(f'{LJoyHorz:.2f}') != 0.0):
-        #Left motor forwards, Right motor downwards
-        motors[5] += LJoyHorz
-        motors[2] += -LJoyHorz
+    vX = float(RJoyHorz)
+    vY = float(RJoyVert)
+
+    angle =  float((RBumper - LBumper) * 45.0)
+
+    #if clockwise
+    #NOTE: This doesn't rotate the ROV, it just changes values to rotate the motors. This needs to be changed.
+    if (angle >= 5.0):
+        vXA = float(vX * cos(angle)) - float(vY * sin(angle))
+        vYA = float(vX * sin(angle)) + float(vY * cos(angle))
+    elif (angle <= -5.0): #counter-clockwise
+        vXA = float(vX * cos(angle)) + float(vY * sin(angle))
+        vYA = float(-vX * sin(angle)) + float(vY  * cos(angle))
+    else:
+        vXA = vX
+        vYA = vY
+
+    #mag = sqrt((vX ** 2) + (vY ** 2))
+    #if(round(mag, 2) != 0):
+    #    vX /= mag
+    #    vY /= mag
+
+
+    if(round(LJoyVert, 2) != 0.0):
+        motors[5] += LJoyVert
+        motors[2] += LJoyVert
+
+    if(round(LJoyHorz, 2) != 0.0):
+        motors[5] += -LJoyHorz
+        motors[2] += LJoyHorz
 
     #left joystick Y would be pitch, don't believe we can change pitch though
 
-    #right joystick X is yaw
-    if(float(f'{RJoyHorz:.2f}') != 0.0):
-        motors[1] += -RJoyHorz
-        motors[4] += -RJoyHorz
-        motors[0] += RJoyHorz
-        motors[3] += RJoyHorz
+    #right joystick is X-Y plane
+    #X direction
+    if(round(vX, 2) != 0.0):
+        motors[1] += vXA
+        motors[4] += -vXA
+        motors[0] += -vXA
+        motors[3] += vXA
 
-    #right joystick Y is forward/backwards
-    if(float(f'{RJoyVert:.2f}') != 0.0):
-        motors[0] += RJoyVert
-        motors[1] += RJoyVert
-        motors[3] += -RJoyVert
-        motors[4] += -RJoyVert
+    #right joystick Y is Y
+    if(round(vY, 2) != 0.0):
+        motors[0] += -vYA
+        motors[1] += -vYA
+        motors[3] += vYA
+        motors[4] += vYA
 
     #triggers control altitude
-    motors[2] += RBumper - LBumper
-    motors[5] += RBumper - LBumper
+    #motors[2] += RBumper - LBumper
+    #motors[5] += RBumper - LBumper
 
-    fmotors = [int(i)*255 for i in motors]
+    #print(motors)
+
+    fmotors = [max(min(int(i*127), 127), -127) for i in motors]
 
     #Byte loses percision but means less data sending, so overall worth it.
     #To-do, ensure separately that this will work.
